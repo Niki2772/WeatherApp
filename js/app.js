a57fb6a8e9fb634c7fcf4be5f85fc4683a606776
Niki2772 <klokova.nika17@gmail.com>
2019-02-13 16:35:09 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      initialSlide: 2,
+      centerMode: true,
+      centerPadding: '185px',
+      autoPlay:true,
+      
+    });
+
+
+    });
+            
